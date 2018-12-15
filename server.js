a89c06e3098b37d0fd19471246b492a370d96f09
Walter Ashbrook <washbrook.wa@gmail.com>
2018-12-15 11:38:27 -0700

    Add projects page.

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    message: 'Portfolio Page Here!'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to fulfill this request.'
